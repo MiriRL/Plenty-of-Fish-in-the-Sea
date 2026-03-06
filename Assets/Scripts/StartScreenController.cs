307dff3592d4f0a729c56b43f42fc21493d06998
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2026-03-06 17:56:21 -0600

    added title screen, background assets

---
+++ Assets/Scripts/StartScreenController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StartScreenController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onStartClick()
+    {
+        Debug.Log("Start game clicked");
+    }
+}
