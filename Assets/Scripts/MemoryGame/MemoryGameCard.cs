fdc05b2faa1a9610d558e198f149c1cb733d5e78
samjkenney <129895815+samjkenney@users.noreply.github.com>
2026-03-31 11:00:55 -0500

    more MG progress

--- Assets/Scripts/MemoryGame/MemoryGameCard.cs
+++ Assets/Scripts/MemoryGame/MemoryGameCard.cs
@@ -3,35 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MemoryGameCard", menuName = "Scriptable Objects/MemoryGameCard")]
 public class MemoryGameCard : ScriptableObject
 {
-    //represents the text on the card
-  [SerializeField] string text; 
   //the matching card to this card
-  [SerializeField] MemoryGameCard match;
-  [SerializeField] Sprite face;
-  [SerializeField] Sprite back;
+  public MemoryGameCard match;
+  public GameObject face;
+  public GameObject back;
   public bool flippable;
-
-  [SerializeField] GameEvent flipEvent;
-
-  //Front utilizes the text - 
-
-    //onClick:
-    //if flippable:
-      //flip to front face
-      // display text
-      // flippable = false
-      // flipEvent.Raise();
-
-    //public void FlipBack(){
-      //remove text
-      //flip to back face 
-      // set flippable to true
-    //}
-    
-
-    public bool CheckMatch(MemoryGameCard otherCard)
-    {
-      return (match == otherCard);
-    }
+  public GameEvent flipEvent;
     
 }
