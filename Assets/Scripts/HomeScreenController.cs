9c9c716742a4694af79425ff15d9195e7399f32b
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2026-04-02 10:40:32 -0500

    menu toggles on start date
    also dialogue slows after a ...

--- Assets/Scripts/HomeScreenController.cs
+++ Assets/Scripts/HomeScreenController.cs
@@ -3,25 +3,27 @@ using UnityEngine;
 
 public class HomeScreenController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Dialogue that will play on screen start whenever it's loaded
+    public DialogueTree startDialogue;
     [SerializeField] private Canvas dialogueCanvas;
-    [SerializeField] private DialogueTree dialogueTree;
     [SerializeField] private CharacterSpriteController characterSpriteController;
-    [Tooltip("The name of the JSON file which must be located in Resources/Dialogues")]
-    [SerializeField] private string DialogueJSONFileName;
-    [SerializeField] private bool writeTreeToJSON;
+    // [Tooltip("The name of the JSON file which must be located in Resources/Dialogues")]
+    // [SerializeField] private string DialogueJSONFileName;
+    // [SerializeField] private bool writeTreeToJSON;
     
     void Start()
     {
         DialogueManager dialogueManager = dialogueCanvas.GetComponent<DialogueManager>();
-        // dialogueManager.StartDialogue(dialogueTree);
-        DialogueFromJSON dialogueFromJSON = ScriptableObject.CreateInstance<DialogueFromJSON>();
-        dialogueFromJSON.LoadDialogueTree(characterSpriteController.talkingCharacter, DialogueJSONFileName);
-        dialogueManager.StartDialogue(dialogueFromJSON.dialogueTree);
+        dialogueManager.StartDialogue(startDialogue);
+        // Code to start the dialogue from a JSON instead. Switch out for the line above if prefered.
+        // DialogueFromJSON dialogueFromJSON = ScriptableObject.CreateInstance<DialogueFromJSON>();
+        // dialogueFromJSON.LoadDialogueTree(characterSpriteController.talkingCharacter, DialogueJSONFileName);
+        // dialogueManager.StartDialogue(dialogueFromJSON.dialogueTree);
         
-        if (writeTreeToJSON) {
-            // DialogueFromJSON dialogueFromJSON = ScriptableObject.CreateInstance<DialogueFromJSON>();
-            dialogueFromJSON.MakeDialogueJSON(dialogueTree, "testTree.json");
-        }
+        // 
+        // if (writeTreeToJSON) {
+        //     // DialogueFromJSON dialogueFromJSON = ScriptableObject.CreateInstance<DialogueFromJSON>();
+        //     dialogueFromJSON.MakeDialogueJSON(dialogueTree, "testTree.json");
+        // }
     }
 }
