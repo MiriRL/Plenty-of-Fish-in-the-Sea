e594163c1abcb3f0bdbeb8d4d785b48d5dc2dd68
aweiss-mac28 <aweiss1@macalester.edu>
2026-05-07 01:05:05 -0500

    Warning flashes, fish can't escape the boundaries

--- Assets/Scripts/FishRace/FishRacingFishMouse.cs
+++ Assets/Scripts/FishRace/FishRacingFishMouse.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -24,6 +25,8 @@ public class FishRacingFishMouse : MonoBehaviour
     {
         Vector3 mousePositionRel = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
+        if(transform.position.x<3.5f && transform.position.x>-3.5f  && transform.position.y<4.5f &&  transform.position.y>-4.5f){
+        Debug.Log(mousePositionRel.y);
         Vector2 direction = mousePositionRel - transform.position;
         float targetAngle = Vector2.SignedAngle(Vector2.up, direction);
         //transform.eulerAngles = new Vector3 (0, 0, angle);
@@ -31,9 +34,10 @@ public class FishRacingFishMouse : MonoBehaviour
         angle = Mathf.SmoothDampAngle(angle, targetAngle, ref currentVelocityAngle, smoothTime, maxTurnSpeed);
         transform.eulerAngles = new Vector3(0, 0, angle);
         
-
+    
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition += ((Vector2)transform.position - mousePosition).normalized * mindistance;
         transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, moveSpeed);
+        }
     }
 }
--- Assets/Scripts/FishRace/WarningController.cs
+++ Assets/Scripts/FishRace/WarningController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WarningController : MonoBehaviour
 {
+    public float interval;
+    private float count;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(WaitForDestroy());
+        count = 0;
+        InvokeRepeating("DisappearanceLogic", 0, interval);
     }
 
     // Update is called once per frame
@@ -15,9 +19,21 @@ public class WarningController : MonoBehaviour
         
     }
 
-    private IEnumerator WaitForDestroy()
-    {
-        yield return new WaitForSeconds(3);
-        Object.Destroy(this.gameObject);
-    }
+    void DisappearanceLogic() 
+{
+     if(this.gameObject.activeSelf) 
+     {
+         this.gameObject.SetActive(false);
+         count+=1;
+     }
+     else
+     {
+         this.gameObject.SetActive(true);
+         count+=1;
+     }
+     if(count > 5)
+        {
+           Object.Destroy(this.gameObject); 
+        }
+}
 }
